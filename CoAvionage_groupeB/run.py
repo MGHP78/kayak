13b29967ac5c1ef402e39604216c2ce3213e752d
MGHP78 <66566905+MGHP78@users.noreply.github.com>
2024-04-22 12:01:06 +0200

    Co-authored-by: jadecarpenter <jadecarpenter@users.noreply.github.com> Co-authored-by: sushinobi <sushinobi@users.noreply.github.com>

--- CoAvionage_groupeB/run.py
+++
@@ -1,8 +0,0 @@
-from CoAvionage_groupeB.config import DEBUG, WEB_SERVER
-from CoAvionage_groupeB.views import app
-if __name__ == '__main__':
-    app.run(
-        host = WEB_SERVER['host'],
-        port=  WEB_SERVER['port'],
-        debug=DEBUG
-)
