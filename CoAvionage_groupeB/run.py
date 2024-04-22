a7c521cf8cfaca2a00f1933d4e2118b90b0b4a45
sushinobi <166594736+sushinobi@users.noreply.github.com>
2024-04-22 11:53:53 +0200

    modif du run et view

--- CoAvionage_groupeB/run.py
+++ CoAvionage_groupeB/run.py
@@ -1,5 +1,5 @@
-from myApp.config import DEBUG, WEB_SERVER
-from myApp.views import app
+from CoAvionage_groupeB.config import DEBUG, WEB_SERVER
+from CoAvionage_groupeB.views import app
 if __name__ == '__main__':
     app.run(
         host = WEB_SERVER['host'],
