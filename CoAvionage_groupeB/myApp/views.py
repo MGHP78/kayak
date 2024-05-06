a0e4c876e630767b95382333cd8962b26c75e70b
MGHP78 <66566905+MGHP78@users.noreply.github.com>
2024-05-06 10:36:43 +0200

    login, compte, ajout user
    Co-authored-by: sushinobi <sushinobi@users.noreply.github.com>

--- CoAvionage_groupeB/myApp/views.py
+++ CoAvionage_groupeB/myApp/views.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template 
+from flask import Flask, render_template, request, redirect, session
 app = Flask(__name__) 
 app.template_folder = "template" 
 app.static_folder = "static" 
@@ -15,3 +15,25 @@ def sgbd():
     }
     params = f.messageInfo(params)
     return render_template("sgbd.html", **params)
+
+# ajout d'un membre
+@app.route("/addMembre"
+, methods=['POST'])
+def addMembre():
+# réception des données du formulaire
+    nom = request.form['nom']
+    prenom = request.form['prenom']
+    mail = request.form['mail']
+    login = request.form['login']
+    motPasse = request.form['mdp']
+    statut = request.form['statut']
+    avatar = request.form['avatar']
+    lastId = bdd.add_membreData(nom, prenom, mail,
+    login, motPasse, statut, avatar)
+    print(lastId) # dernier id créé par le serveur de BDD
+    
+    if "errorDB" not in session:
+        session["infoVert"]="Nouveau membre inséré"
+    else:
+        session["infoRouge"]="Problème ajout utilisateur"
+    return redirect("/sgbd")
